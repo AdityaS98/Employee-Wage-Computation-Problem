058954ae3d56317ba82f4a7a18a6bb9d5df1bc67
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-14 20:15:35 +0530

    Adding a programm for validating name

---
+++ eclipse-workspace/GambleGame/src/com/game/GameOfGamble.java
@@ -0,0 +1,23 @@
+package com.game;
+/**
+ * 
+ * @author DELL
+ *
+ */
+public class GameOfGamble {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * The game will start with 100 $ total amount and at every bet 1$ will be at stake   
+		 */
+		
+		int totalAmount=100;
+		int Bet=1;
+
+	}
+
+}
